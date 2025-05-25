d7985eaa39fff815706ea2e73a4d821dffe998a2
DevanshMishra123 <mishra24devansh@gmail.com>
2025-05-25 14:03:59 +0530

    changed file

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const cors = require('cors');
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
 
-const io =  require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET','POST']
-    }
-})
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-    res.send("Server is running");
-})
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-io.on('connection', (socket) => {
-    socket.emit('me', socket.id)
+io.on("connection", (socket) => {
+  console.log("New connection: ", socket.id);
+  socket.emit("me", socket.id);
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit("callended")
-    })
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
 
-    socket.on('calluser', ({ userToCall, signalData, from, name}) => {
-        io.to(userToCall).emit("calluser", {signal: signalData, from, name})
-    })
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", {
+      signal: signalData,
+      from,
+      name,
+    });
+  });
 
-    socket.on('answercall', (data) => {
-        io.to(data.to).emit('callaccepted', data.signal);
-    })
-})
+  socket.on("answercall", ({ to, signal }) => {
+    io.to(to).emit("callAccepted", signal);
+  });
+});
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-
